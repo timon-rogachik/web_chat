34cf074270f5717095974bd68d84aaedd0d2124c
timon-rogachik <tim.rogachik@gmail.com>
2021-08-03 11:55:52 +0300

    releze vers8

--- web_chat/settings.py
+++ web_chat/settings.py
@@ -133,10 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/ 'static'
+# STATIC_ROOT = BASE_DIR/ 'static'
 STATICFILES_DIRS = [
     BASE_DIR/ 'asets',
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
